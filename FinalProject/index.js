277fd88f46b1657a71ebac3201c3dc8b9bbbac91
nachiket1234-cell <164024788+Nachiket-Palekar@users.noreply.github.com>
2024-03-29 03:37:05 +0530

    peor

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -3,25 +3,31 @@
 var mysql = require('mysql');
             var express = require('express');
             var app= express()
-            var con=mysql.createConnection({
+            const path = require('path'); 
+            var pool =mysql.createConnection({
               Host: 'localhost',
               user: 'root',
               password:'root123',
+              database:'wp'
             });
-            con.connect( function(err) {
-                if(err)throw err; 
-                console.log("Connected!");
-                var qur = "SELECT `proj_login`.`username`,`proj_login`.`pass`FROM `wp`.`proj_login`";
-            con.query(qur,function(err,result){
-                if(err)throw err; 
-                console.log(result);
-                // var user="70472200139";
-                // var pass="nachiket11";
-                //     if(user != result.username || pass != result.pass)
-                //         {if(err)throw err;}
-                //     else
-                //     {console.log("match found");}
-                    
+
+            app.use(express.static(path.join(__dirname, 'public')));
+            app.post('/login', async (req, res) => {
+              const { Username, Password } = req.body;
+          
+              try {
+                  const result = await pool.query('SELECT * FROM `wp`.`proj_login` WHERE username = $1 AND password = $2', [Username, Password]);
                   
-                })
-      });
+                  if (result.rows.length > 0) {
+                      res.send('Login successful');
+                  } else {
+                      res.send('Invalid username or password');
+                  }
+              } catch (err) {
+                  console.error('Error executing query', err);
+                  res.status(500).send('Internal Server Error');
+              }
+          });
+          app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html');
+        });
