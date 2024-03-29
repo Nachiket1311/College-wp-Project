46de789340b1306e692fa934bef05f74e28120ad
nachiket1234-cell <164024788+Nachiket-Palekar@users.noreply.github.com>
2024-03-29 19:50:35 +0530

    project

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -3,31 +3,40 @@
 var mysql = require('mysql');
             var express = require('express');
             var app= express()
-            const path = require('path'); 
-            var pool =mysql.createConnection({
+            const bodyParser = require("body-parser");
+            const encoder = bodyParser.urlencoded();
+            var con =mysql.createConnection({
               Host: 'localhost',
               user: 'root',
               password:'root123',
               database:'wp'
             });
 
-            app.use(express.static(path.join(__dirname, 'public')));
-            app.post('/login', async (req, res) => {
-              const { Username, Password } = req.body;
-          
-              try {
-                  const result = await pool.query('SELECT * FROM `wp`.`proj_login` WHERE username = $1 AND password = $2', [Username, Password]);
-                  
-                  if (result.rows.length > 0) {
-                      res.send('Login successful');
-                  } else {
-                      res.send('Invalid username or password');
-                  }
-              } catch (err) {
-                  console.error('Error executing query', err);
-                  res.status(500).send('Internal Server Error');
-              }
-          });
-          app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/index.html');
-        });
+            con.connect(function(error){
+                if (error) throw error
+                else console.log("connected to the database successfully!")
+            });
+            app.get("/",function(req,res){
+                res.sendFile(__dirname + "/login.html");
+            })
+            app.post("/",encoder, function(req,res){
+                var username = req.body.username;
+                var password = req.body.password;
+            
+                connection.query("select * from loginuser where user_name = ? and user_pass = ?",[username,password],function(error,results,fields){
+                    if (results.length > 0) {
+                        res.redirect("/welcome");
+                    } else {
+                        res.redirect("/");
+                    }
+                    res.end();
+                })
+            })
+        // when login is success
+app.get("/welcome",function(req,res){
+    res.sendFile(__dirname + "/welcome.html")
+})
+
+
+// set app port 
+app.listen(1000);
