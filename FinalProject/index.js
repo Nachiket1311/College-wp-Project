df0e83f9c8c8fc793f2ffb8567832312d840ee6d
nachiket1234-cell <164024788+Nachiket-Palekar@users.noreply.github.com>
2024-03-30 09:20:50 +0530

    project

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -1,42 +1,45 @@
-// var user=document.inp1.value;
-// var pass=document.inp2.value;
 var mysql = require('mysql');
-            var express = require('express');
-            var app= express()
-            const bodyParser = require("body-parser");
-            const encoder = bodyParser.urlencoded();
-            var con =mysql.createConnection({
-              Host: 'localhost',
-              user: 'root',
-              password:'root123',
-              database:'wp'
-            });
+var express = require('express');
+var app = express();
+const bodyParser = require("body-parser");
+const encoder = bodyParser.urlencoded();
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    database: 'wp'
+});
 
-            con.connect(function(error){
-                if (error) throw error
-                else console.log("connected to the database successfully!")
-            });
-            app.get("/",function(req,res){
-                res.sendFile(__dirname + "/login.html");
-            })
-            app.post("/",encoder, function(req,res){
-                var username = req.body.username;
-                var password = req.body.password;
-            
-                connection.query("select * from loginuser where user_name = ? and user_pass = ?",[username,password],function(error,results,fields){
-                    if (results.length > 0) {
-                        res.redirect("/welcome");
-                    } else {
-                        res.redirect("/");
-                    }
-                    res.end();
-                })
-            })
-        // when login is success
-app.get("/welcome",function(req,res){
-    res.sendFile(__dirname + "/welcome.html")
-})
+con.connect(function(error) {
+    if (error) throw error;
+    else console.log("connected to the database successfully!");
+});
 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/login.html");
+});
+
+app.post("/", encoder, function(req, res) {
+    var username = req.body.user;
+    var password = req.body.pass;
+
+    con.query("SELECT * FROM login WHERE username = ? AND pass = ?", [username, password], function(error, results, fields) {
+        if (error) throw error;
+        
+        if (results.length > 0) {
+            res.redirect("/welcome");
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+// when login is successful
+app.get("/welcome", function(req, res) {
+    res.sendFile(__dirname + "/welcome.html");
+});
 
 // set app port 
-app.listen(1000);
+app.listen(1100, function() {
+    console.log('App listening on port 1100');
+});
